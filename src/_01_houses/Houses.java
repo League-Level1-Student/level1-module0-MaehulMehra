7a58267dbdefc097f8699c8dec7968e62335fd5a
maehu <maehu@192.168.1.14>
2020-12-10 16:54:20 -0800

    house runner

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,30 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
-	public void run() {
-		
+	Robot houseMaker = new Robot (45, 400);
+	public void run() {	
+		for (int i = 0; i <10; i++) {
+		makeHouse (100);
+		}
+	}
+	void makeHouse (int height) {
+			houseMaker.setPenColor(0, 0, 0);
+			houseMaker.setSpeed(25);
+			houseMaker.penDown();
+			houseMaker.setAngle(0);
+			houseMaker.move(height);
+			houseMaker.turn(90);
+			houseMaker.move(25);
+			houseMaker.turn(90);
+			houseMaker.move(height);
+			houseMaker.turn(-90);
+			houseMaker.setPenColor(Color.green);	
+			houseMaker.move (30);
+			houseMaker.turn (-90);
 	}
 }
+
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -25,7 +25,7 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address”)
+		// Copy Image Address
 
 		// 2. create a variable of type "Component" that will hold your image
 
