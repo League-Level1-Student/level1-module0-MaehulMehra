4915670fd0956cbd84b44a0659d84563615aacde
maehu <maehu@192.168.1.14>
2020-12-10 16:54:32 -0800

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,15 +1,27 @@
 package _01_houses;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	Robot houseMaker = new Robot (45, 400);
 	public void run() {	
+		Random random = new Random ();
 		for (int i = 0; i <10; i++) {
-		makeHouse (100);
+		int size = random.nextInt(3);
+		if (size == 0){
+			makeHouse ("small");
 		}
+		else if (size == 1) {
+			makeHouse ("medium");
+		}
+		else {
+			makeHouse ("large");
+		}
+		}
+		houseMaker.hide();
 	}
 	void makeHouse (int height) {
 			houseMaker.setPenColor(0, 0, 0);
@@ -25,6 +37,17 @@ public class Houses {
 			houseMaker.setPenColor(Color.green);	
 			houseMaker.move (30);
 			houseMaker.turn (-90);
+	}
+	void makeHouse (String height) {
+		if (height.equalsIgnoreCase("small")) {
+			makeHouse (60);
+		}
+		else if (height.equalsIgnoreCase("medium")) {
+			makeHouse (120);
+		}
+		else if (height.equalsIgnoreCase("large")) {
+			makeHouse (250);
+		}
 	}
 }
 
